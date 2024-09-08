568b905bee77209080b49614fb48d58d530c2b62
Andre Fukuda <andre.alex.fukuda@gmail.com>
2024-09-08 16:26:58 -0300

    About View

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,9 +3,17 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable{
 
@@ -31,12 +39,34 @@ public class MainViewController implements Initializable{
 	@FXML
 	public void onMenuItemAboutAction() {
 		
-		System.out.println("onMenuItemAboutAction");
+		loadView("/gui/About.fxml");
 	}
 	
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
 		
+	}
+	
+	@FXML
+	private synchronized void loadView(String absoluteName) {
+		
+		try {
+			
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();//janela a ser aberta
+			
+			Scene mainScene = Main.getMainScene();
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent(); //getRoot pega o primeiro elemento da View (no caso da main é o ScrollPane)
+			
+			Node mainMenu = mainVBox.getChildren().get(0);//primeiro filho da Vbox (main menu)
+			mainVBox.getChildren().clear();//limpa o que tem no mainVbox
+			mainVBox.getChildren().add(mainMenu);//adiciona o que tinha antes
+			mainVBox.getChildren().addAll(newVBox.getChildren());//adiciona o conteudo do newVbox
+			
+		} catch (Exception e) {
+
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 }
